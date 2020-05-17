77838f9ad9d337c489ab60c08a7d7d2b3a412012
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-05-17 14:39:52 +0300

    Added check_high_score call when aliens are hit with bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -172,6 +172,7 @@ class AlienInvasion:
             for aliens in collisions.values():
                 self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
+            self.sb.check_high_score()
 
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
