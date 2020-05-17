8fd251db63bff3c6a22a77f2e033e0e79a0914cb
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-05-17 09:51:30 +0300

    Added a method to check for the aliens' edges relative to the screen

--- alien.py
+++ alien.py
@@ -27,11 +27,22 @@ class Alien(Sprite):
         # Store the aliens exact horizontal position.
         self.x = float(self.rect.x)
 
-    def update(self):
+    def check_edges(self):
         """
-        Move the alien to the right.
+        Return True if an alien is at the edge of the screen.
         :return:
         """
 
-        self.x += self.settings.alien_speed
+        screen_rect = self.screen.get_rect()
+
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
+
+    def update(self):
+        """
+        Move the alien to the right or left.
+        :return:
+        """
+
+        self.x += (self.settings.alien_speed * self.settings.fleet_direction)
         self.rect.x = self.x
