c7314cb7ac740b8279cf00cd8c76a51fc0d5e594
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-05-17 11:04:03 +0300

    Added logic to keep the game active as long as the player has at least one ship left

--- alien_invasion.py
+++ alien_invasion.py
@@ -42,10 +42,12 @@ class AlienInvasion:
 
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
-            self._update_screen()
+
+            if self.stats.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+                self._update_screen()
 
     def _check_events(self):
         """
@@ -137,19 +139,23 @@ class AlienInvasion:
         :return:
         """
 
-        # Decrement ships left.
-        self.stats.ships_left -= 1
+        if self.stats.ships_left > 0:
+            # Decrement ships left.
+            self.stats.ships_left -= 1
 
-        # Get rid of any remaining aliens and bullets.
-        self.aliens.empty()
-        self.bullets.empty()
+            # Get rid of any remaining aliens and bullets.
+            self.aliens.empty()
+            self.bullets.empty()
 
-        # Create a new fleet and center the ship.
-        self._create_fleet()
-        self.ship.center_ship()
+            # Create a new fleet and center the ship.
+            self._create_fleet()
+            self.ship.center_ship()
 
-        # Pause.
-        sleep(0.5)
+            # Pause.
+            sleep(0.5)
+
+        else:
+            self.stats.game_active = False
 
     def _update_aliens(self):
         """
