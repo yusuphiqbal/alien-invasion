363583270076cdecd902ab1623a51d85d731fc78
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-05-17 10:11:04 +0300

    Added logic to create new fleet when an old fleet is destroyed

--- alien_invasion.py
+++ alien_invasion.py
@@ -114,6 +114,11 @@ class AlienInvasion:
         # If so get rid of the bullet and alien.
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
 
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet.
+            self.bullets.empty()
+            self._create_fleet()
+
     def _update_aliens(self):
         """
         Check if the fleet is at an edge,
