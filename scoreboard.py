c56738887e9e823e6f1ef77308001512947f495d
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-05-17 14:34:38 +0300

    Added logic to render high score at the top of the screen

--- scoreboard.py
+++ scoreboard.py
@@ -24,6 +24,7 @@ class ScoreBoard:
 
         # Prepare the initial score image
         self.prep_score()
+        self.prep_high_score()
 
     def prep_score(self):
         """
@@ -40,6 +41,21 @@ class ScoreBoard:
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
 
+    def prep_high_score(self):
+        """
+        Turn the high score into a rendered image.
+        :return:
+        """
+
+        high_score = round(self.stats.high_score, -1)
+        high_score_str = "{:,}".format(high_score)
+        self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.settings.bg_color)
+
+        # Center the high score at the top of the screen.
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
+
     def show_score(self):
         """
         Draw score to the screen
@@ -47,3 +63,4 @@ class ScoreBoard:
         """
 
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
