e7fcb89457088fa2316ba7e34e75844da1fcec2d
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-05-17 12:35:57 +0300

    Added logic to deactivate the play button so it does not respond to mouse clicks area when the game is active

--- alien_invasion.py
+++ alien_invasion.py
@@ -80,7 +80,9 @@ class AlienInvasion:
         :return:
         """
 
-        if self.play_button.rect.collidepoint(mouse_pos):
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+
+        if button_clicked and not self.stats.game_active:
             # Reset game statistics.
             self.stats.reset_stats()
             self.stats.game_active = True
