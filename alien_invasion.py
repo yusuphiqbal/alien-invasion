b94d6a02dcd3e595c8f83efa4f508ca7e0ab8265
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-05-17 10:00:48 +0300

    Added methods to check for the fleet's edges and change fleet's direction

--- alien_invasion.py
+++ alien_invasion.py
@@ -112,10 +112,12 @@ class AlienInvasion:
 
     def _update_aliens(self):
         """
-        Update the positions of all aliens in the fleet.
+        Check if the fleet is at an edge,
+        and update the positions of all aliens in the fleet.
         :return:
         """
 
+        self._check_fleet_edges()
         self.aliens.update()
 
     def _create_fleet(self):
@@ -154,6 +156,28 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
+
+    def _check_fleet_edges(self):
+        """
+        Respond appropriately if any aliens have reached the edges.
+        :return:
+        """
+
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """
+        Drop the entire fleet and change the fleet's direction.
+        :return:
+        """
+
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+
+        self.settings.fleet_direction *= -1
 
     def _update_screen(self):
         """
