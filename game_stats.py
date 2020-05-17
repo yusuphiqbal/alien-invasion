95d649d2b232ef7b91343b5580eaed76619cf8a4
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-05-17 11:39:01 +0300

    Changed game_active initially to False when the game starts

--- game_stats.py
+++ game_stats.py
@@ -13,7 +13,7 @@ class GameStats:
         self.reset_stats()
 
         # Start Alien Invasion in an active state
-        self.game_active = True
+        self.game_active = False
 
     def reset_stats(self):
         """
