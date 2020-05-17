d06a5cf421081d873e1e633d3b3c478dae2b10eb
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-05-17 15:09:49 +0300

    Fixed typo in method call

--- scoreboard.py
+++ scoreboard.py
@@ -72,7 +72,7 @@ class ScoreBoard:
         self.level_image = self.font.render(level_str, True, self.text_color, self.settings.bg_color)
 
         # Position the level below th score.
-        self.level_rect = self.level_rect.get_rect()
+        self.level_rect = self.level_image.get_rect()
         self.level_rect.right = self.score_rect.right
         self.level_rect.top = self.score_rect.bottom + 10
 
