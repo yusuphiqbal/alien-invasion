e907b8f321b7a466fc49e9855ed1f91145240ffd
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-05-17 14:34:13 +0300

    Initialized high score to zero

--- game_stats.py
+++ game_stats.py
@@ -15,6 +15,9 @@ class GameStats:
         # Start Alien Invasion in an inactive state.
         self.game_active = False
 
+        # High score should never be reset.
+        self.high_score = 0
+
     def reset_stats(self):
         """
         Initialize statistics that can change during the game.
