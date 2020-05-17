18678cc42ef1ba82e059f322a0dc7e974de012d4
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-05-17 14:51:18 +0300

    Added prep_level calls when we level up or start a new game

--- alien_invasion.py
+++ alien_invasion.py
@@ -93,6 +93,7 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
 
             # Get rid of any remaining alien and bullets.
             self.aliens.empty()
@@ -179,6 +180,10 @@ class AlienInvasion:
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            # Increase level
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _ship_hit(self):
         """
